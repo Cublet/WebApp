8ad083efdf9452919f8a23f4931cf240792a633b
Alastair Paragas <alastairparagas@gmail.com>
2015-07-14 20:24:36 -0400

    Default URL

--- app/app/app.config.js
+++ app/app/app.config.js
@@ -4,22 +4,27 @@
 	var angular = window.angular;
 
 	function config($stateProvider, $compileProvider, $httpProvider, 
-					 AppConfig) {
+					 $urlRouterProvider, AppConfig) {
 
-		$stateProvider.state('cublet', {
-			abstract: true,
-			templateUrl: 'app/app/app.html',
-			url: '',
-			controller: 'AppController'
-		});
+		$stateProvider
+			.state('cublet', {
+				abstract: true,
+				templateUrl: 'app/app/app.html',
+				url: '',
+				controller: 'AppController'
+			})
+			.state('cublet.home', {
+				url: '/home',
+				templateUrl: 'app/app/home.html'
+			});
 		
 		$compileProvider.debugInfoEnabled(AppConfig.optimize);
-		
 		$httpProvider.useApplyAsync(AppConfig.optimize);
+		$urlRouterProvider.otherwise('/home');
 		
 	}
 	config.$inject = ['$stateProvider', '$compileProvider', '$httpProvider', 
-					  'AppConfig'];
+					  '$urlRouterProvider', 'AppConfig'];
 	
 	angular
 		.module('cublet')
